e4180b060d96ffa077c28aa7763f29bcbf4bf4bb
Naomi Kuo <nkb812@gmail.com>
2025-08-17 16:30:14 +0800

    Initial Commit

---
+++ UICard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICard
+//
+//  Created by Naomi Kuo on 2025/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
