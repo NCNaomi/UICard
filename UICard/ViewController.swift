d410250234f0452e5f266e9ee8ebed4965e9947d
Naomi Kuo <nkb812@gmail.com>
2025-08-17 19:32:45 +0800

    na

--- UICard/ViewController.swift
+++ UICard/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Playgrounds
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
 
+#Playground {
+    //let cardImage = UIImage(named: "card")
+    //let cardImageView = UIImageView(image: cardImage)
+    let cardImageView = UIImageView(image: UIImage(named: "card"))
+    cardImageView.backgroundColor = UIColor(red: 150/255, green: 0, blue: 0, alpha: 1)
+}
